7c5adde150c22e33e89cfa62624d13aef5e2b5bf
Gary Chi <gc1753@nyu.edu>
2020-12-19 16:48:41 -0500

    added webhook

--- slackbot.js
+++ slackbot.js
@@ -1,9 +1,13 @@
+const { IncomingWebhook } = require('@slack/webhook');
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 app.use(bodyParser.json())
 app.disable('x-powered-by')
-const port = 3000
+
+const port = process.env.PORT
+const webhookUrl = process.env.WEBHOOK
+const webhook = new IncomingWebhook(webhookUrl);
 
 app.get('/', (req, res) => {
     res.send('OK')
@@ -13,6 +17,19 @@ app.post('/', (req, res) => {
     const response = {}
     if (req.body.challenge) {
         response.challenge = req.body.challenge;
+    }
+    else if (req.body.event) {
+        const event = req.body.event
+        switch (event.type) {
+            case 'message':
+                webhook.send({
+                    text: `Parroting message: ${event.text}`
+                })
+                break;
+            default:
+                console.log(event.type)
+                break;
+        }
     }
     else {
         console.log(req.body);
