ce2a036c1b7db72df39357e7809fbdb1f2fbe943
Gary Chi <gc1753@nyu.edu>
2020-12-19 16:08:39 -0500

    init

---
+++ slackbot.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+app.use(bodyParser.json())
+app.disable('x-powered-by')
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('OK')
+})
+
+app.post('/', (req, res) => {
+    const response = {}
+    if (req.body.challenge) {
+        response.challenge = req.body.challenge;
+    }
+    res.send(response)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
